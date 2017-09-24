39e78e3d78084ae431ad9f7e97da47d3c88cedcf
Patryk <master_sergeant98@interia.pl>
2017-09-25 00:50:28 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+
+var name = prompt('Wpisz swoje imię');  //Tu wpisujemy swoje imię.
+alert('Cześć, ' + name);                //Dzięki temu poleceniu tworzy się okienko witające użytkownika.
+console.log('Cześć, ' + name);          //To polecenie sprawia, że to okienko się pojawia.
